522f9c77320141f4196c8b84c5a1a2b8e5fba754
Jake Klassen <jklassen@circadianrisk.com>
2024-10-18 15:25:59 -0400

    logic to fail

--- scripts/check_concurrent_runs.js
+++ scripts/check_concurrent_runs.js
@@ -3,8 +3,8 @@ async function main() {
 	const repo = process.argv[2];
 	const workflow_id = process.argv[3];
 
-	const results = await fetch(
-		`https://api.github.com/repos/jakeklassen/${repo}/actions/workflows/${workflow_id}/runs`,
+	const { total_count } = await fetch(
+		`https://api.github.com/repos/jakeklassen/${repo}/actions/workflows/${workflow_id}/runs?status=queued&status=in_progress&per_page=1`,
 		{
 			headers: {
 				Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
@@ -12,7 +12,12 @@ async function main() {
 		},
 	).then((res) => res.json());
 
-	console.log(results);
+	if (total_count > 0) {
+		console.error("Concurrent runs detected");
+		process.exit(1);
+	}
+
+	console.log("No concurrent runs detected");
 }
 
 main();
